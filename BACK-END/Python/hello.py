49939e5bca34794e3bd25a7d2a9c94a4c35c7e46
GAJAN <sivagajan96in@gmail.com>
2026-05-23 22:30:10 +0530

    updated

---
+++ BACK-END/Python/hello.py
@@ -0,0 +1,21 @@
+#If you want to print multiple words on the same line, you can use the end parameter:
+# print("Hello World!", end="-")
+# print("I will print on the same line.")
+
+# You can combine text and numbers in one output by separating them with a comma:
+# print("hey", "I am", "Gajan", 29, "years old")
+
+# x = int(3)
+# print(type(x))
+
+# x, y,z = ["apple", "banana", "cherry"]
+# print(y)
+
+# To create a global variable inside a function, you can use the global keyword.
+# def myfunc():
+#   global x
+#   x = "fantastic"
+
+# myfunc()
+
+# print("Python is " + x)
