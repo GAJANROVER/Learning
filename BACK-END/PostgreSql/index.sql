3d557c82bd68dea8e94c645ffb55f84c71adcb03
gajan <gajandigitweb@gmail.com>
2026-05-25 17:59:04 +0530

    Create index.sql
    added postgresql  learning

---
+++ BACK-END/PostgreSql/index.sql
@@ -0,0 +1,43 @@
+-- In this example, we used the concatenation operator || to concatenate the first name, space, and last name of every customer.
+-- If a column alias contains one or more spaces, you need to surround it with double quotes like this:
+-- SELECT
+--     first_name || ' ' || last_name "full name",
+-- 	email
+-- FROM
+--     customer;
+
+
+
+-- Because the ORDER BY clause is evaluated after the SELECT clause, the column alias len is available and can be used in the ORDER BY clause.
+-- SELECT
+--   first_name,
+--   LENGTH(first_name) len
+-- FROM
+--   customer
+-- ORDER BY
+--   len DESC;
+
+
+
+-- To place NULL before other non-null values, you use the NULLS FIRST option:
+-- SELECT
+--   num
+-- FROM
+--   sort_demo
+-- ORDER BY
+--   num NULLS FIRST;
+
+
+-- DISTINCT Removes all duplicate rows from the result set — every column must match for a row to be considered a duplicate.
+SELECT DISTINCT name, subject, score  from student_scores order by "name";
+
+-- DISTINCT ON (expression) A PostgreSQL-specific extension that keeps only the first row for each unique value of the specified expression(s), while still letting you select other columns.
+SELECT
+  DISTINCT ON (name) name,
+  subject,
+  score
+FROM
+  student_scores
+ORDER BY
+  name,
+  score DESC;
