9fac4b60fe234238c6ac645bd4a47ef52a478c00
gajan <gajandigitweb@gmail.com>
2026-06-01 18:01:25 +0530

    june 1 2026 learning done posdtgresql

--- BACK-END/PostgreSql/index.sql
+++ BACK-END/PostgreSql/index.sql
@@ -29,15 +29,54 @@
 
 
 -- DISTINCT Removes all duplicate rows from the result set — every column must match for a row to be considered a duplicate.
-SELECT DISTINCT name, subject, score  from student_scores order by "name";
+-- SELECT DISTINCT name, subject, score  from student_scores order by "name";
 
--- DISTINCT ON (expression) A PostgreSQL-specific extension that keeps only the first row for each unique value of the specified expression(s), while still letting you select other columns.
-SELECT
-  DISTINCT ON (name) name,
-  subject,
-  score
-FROM
-  student_scores
-ORDER BY
-  name,
-  score DESC;
+-- -- DISTINCT ON (expression) A PostgreSQL-specific extension that keeps only the first row for each unique value of the specified expression(s), while still letting you select other columns.
+-- SELECT
+--   DISTINCT ON (name) name,
+--   subject,
+--   score
+-- FROM
+--   student_scores
+-- ORDER BY
+--   name,
+--   score DESC;
+
+
+-- SELECT
+--     film_id,
+--     title
+-- FROM
+--     film
+-- ORDER BY
+--     title
+-- OFFSET 5 ROW
+-- FETCH FIRST 5 ROW ONLY;
+
+-- To match the values in the payment_date column with a list of dates, you need to cast them to date values that have the date part only.
+
+-- To do that you use the :: cast operator:
+
+-- SELECT
+--   payment_id,
+--   amount,
+--   payment_date
+-- FROM
+--   payment
+-- WHERE
+--   payment_date::date IN ('2007-02-15', '2007-02-16');
+
+
+-- inner join 
+
+-- SELECT
+--   customer_id,
+--   first_name,
+--   last_name,
+--   amount,
+--   payment_date
+-- FROM
+--   customer
+--   INNER JOIN payment USING(customer_id)
+-- ORDER BY
+--   payment_date;
